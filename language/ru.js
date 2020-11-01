a2808a04e252324c530bfeab46ef9797ea915a7f
Maxim Shibanov <50351588+MShiba264@users.noreply.github.com>
2020-11-01 14:59:04 +0800

    Create ru.js
    Added russian translataion

---
+++ language/ru.js
@@ -0,0 +1,201 @@
+{
+  "semantics": [
+    {
+      "label": "Добавить обложку книги",
+      "description": "Обложка показывает информацию о книге перед её открытием"
+    },
+    {
+      "label": "Обложка",
+      "fields": [
+        {
+          "label": "Описание обложки",
+          "description": "Этот текст будет описанием вашей книги."
+        },
+        {
+          "label": "Изображение обложки",
+          "description": "Необязательное фоновое изображение для введения."
+        },
+        {
+          "label": "Альтернативный текст для изображения обложки",
+          "description": "Альтернативный текст для изображения обложки"
+        }
+      ]
+    },
+    {
+      "label": "Страницы",
+      "entity": "Страница",
+      "widgets": [
+        {
+          "label": "По умолчанию"
+        }
+      ],
+      "field": {
+        "label": "Item",
+        "fields": [
+          {
+            "label": "Страница"
+          }
+        ]
+      }
+    },
+    {
+      "label": "Настройки поведения",
+      "fields": [
+        {
+          "label": "Отображать оглавление по умолчанию",
+          "description": "Если эта опция задействована, оглавление будет отображаться при открытии книги"
+        },
+        {
+          "label": "Отображать индикаторы выполнения",
+          "description": "Если эта опция задействована, пользователю будут показываться индикаторы о выполнении страницы"
+        },
+        {
+          "label": "Отображать автоматический прогресс",
+          "description": "Если эта опция задействована,страница без задач будет отображаться выполненной, когда будет просмотрена. Страница с задачами - когда все задачи будут выполнены. A page with tasks when all tasks are done. If disabled there will be a button at the bottom of every page for the user to click when done with the page."
+        },
+        {
+          "label": "Display summary",
+          "description": "When enabled the user can see a summary and submit the progress/answers"
+        }
+      ]
+    },
+    {
+      "label": "Translation for \"Read\"",
+      "default": "Read"
+    },
+    {
+      "label": "Translation for \"Display 'Table of contents'\"",
+      "default": "Display 'Table of contents'"
+    },
+    {
+      "label": "Translation for \"Hide 'Table of contents'\"",
+      "default": "Hide 'Table of contents'"
+    },
+    {
+      "label": "Translation for \"Next page\"",
+      "default": "Next page"
+    },
+    {
+      "label": "Translation for \"Previous page\"",
+      "default": "Previous page"
+    },
+    {
+      "label": "Translation for \"Page completed!\"",
+      "default": "Page completed!"
+    },
+    {
+      "label": "Translation for \"@pages of @total completed\" (@pages and @total will be replaced by actual values)",
+      "default": "@pages of @total completed"
+    },
+    {
+      "label": "Translation for \"Incomplete page\"",
+      "default": "Incomplete page"
+    },
+    {
+      "label": "Translation for \"Navigate to the top\"",
+      "default": "Navigate to the top"
+    },
+    {
+      "label": "Translation for \"I have finished this page\"",
+      "default": "I have finished this page"
+    },
+    {
+      "label": "Fullscreen button label",
+      "default": "Fullscreen"
+    },
+    {
+      "label": "Exit fullscreen button label",
+      "default": "Exit fullscreen"
+    },
+    {
+      "label": "Page progress in book",
+      "description": "\"@count\" will be replaced by page count, and \"@total\" with the total number of pages",
+      "default": "@count of @total pages"
+    },
+    {
+      "label": "Interaction progress",
+      "description": "\"@count\" will be replaced by interaction count, and \"@total\" with the total number of interactions",
+      "default": "@count of @total interactions"
+    },
+    {
+      "label": "Translation for \"Submit report\"",
+      "default": "Submit Report"
+    },
+    {
+      "label": "Label for \"restart\" button",
+      "default": "Restart"
+    },
+    {
+      "label": "Summary header",
+      "default": "Summary"
+    },
+    {
+      "label": "Translation for \"All interactions\"",
+      "default": "All interactions"
+    },
+    {
+      "label": "Translation for \"Unanswered interactions\"",
+      "default": "Unanswered interactions"
+    },
+    {
+      "label": "Score",
+      "description": "\"@score\" will be replaced with current score, and \"@maxscore\" will be replaced with max achievable score",
+      "default": "@score / @maxscore"
+    },
+    {
+      "label": "Per page interactions completion",
+      "description": "\"@left\" will be replaced with remaining interactions, and \"@max\" will be replaced with total number of interactions on the page",
+      "default": "@left of @max interactions completed"
+    },
+    {
+      "label": "Translation for \"No interactions\"",
+      "default": "No interactions"
+    },
+    {
+      "label": "Translation for \"Score\"",
+      "default": "Score"
+    },
+    {
+      "label": "Label for \"Summary & submit\" button",
+      "default": "Summary & submit"
+    },
+    {
+      "label": "Translation for \"You have not interacted with any pages.\"",
+      "default": "You have not interacted with any pages."
+    },
+    {
+      "label": "Translation for \"You have to interact with at least one page before you can see the summary.\"",
+      "default": "You have to interact with at least one page before you can see the summary."
+    },
+    {
+      "label": "Translation for \"Your answers are submitted for review!\"",
+      "default": "Your answers are submitted for review!"
+    },
+    {
+      "label": "Summary progress label",
+      "default": "Book progress"
+    },
+    {
+      "label": "Interactions progress label",
+      "default": "Interactions progress"
+    },
+    {
+      "label": "Total score label",
+      "default": "Total score"
+    },
+    {
+      "label": "Accessibility texts",
+      "fields": [
+        {
+          "label": "Page progress textual alternative",
+          "description": "An alternative text for the visual page progress. @page and @total variables available.",
+          "default": "Page @page of @total."
+        },
+        {
+          "label": "Label for expanding/collapsing navigation menu",
+          "default": "Toggle navigation menu"
+        }
+      ]
+    }
+  ]
+}
